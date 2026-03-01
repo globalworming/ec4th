4ffe2eeaf2dfce15f8468383fda866fb2831e1f4
Jens Wilke <jw_github@headissue.com>
2026-03-01 20:45:18 +0100

    add some notes in the readme, etc. simmulator is printing hello world

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## Recommended VC Code Plugins
+
+ext install fttx.language-forth
+Ctags Companion
+
+
+    apt-get install simavr binutils-avr
+
+
+Work in progress
+
+bash run.sh
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 gforth -e "fpath= .|`pwd`" avrtest.fs -e bye
+avr-objcopy   --change-section-vma .data=0x0000   --rename-section .data=.text   avr-plain.elf avr.elf
+simavr -m atmega328p -f 16000000 avr.elf
