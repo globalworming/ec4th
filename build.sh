3697422b994663c7ee14df36c2c51e6747c5de77
Jens Wilke <jw_github@headissue.com>
2026-04-04 14:07:35 +0200

    remove elf from build, not needed for simavr

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ mkdir -p output
 
 gforth -e "fpath= .|`pwd`" build.fs -e bye 
 t=output/ec4th-arduino-nano-regular
-avr-objcopy -I binary -O elf32-avr --change-section-vma .data=0x0000 --rename-section .data=.text $t.bin $t.elf
+# elf not needed, simavr supports hex input also
+# avr-objcopy -I binary -O elf32-avr --change-section-vma .data=0x0000 --rename-section .data=.text $t.bin $t.elf
 avr-objcopy -O ihex -R .eeprom $t.elf $t.hex
 
 # simavr -m atmega328p -f 16000000 $t.elf
