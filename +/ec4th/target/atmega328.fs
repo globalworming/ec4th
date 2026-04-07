76f716ec0b524535a1ffa86a4d1a64ee8a6f8e48
Jens Wilke <jw_github@headissue.com>
2026-04-07 16:46:31 +0800

    cleanup

--- +/ec4th/target/atmega328.fs
+++ +/ec4th/target/atmega328.fs
@@ -13,20 +13,6 @@ Label error-ip		0 ,
 Label init-ramend return-stack-init addr>data ,
 Label init-dataStack data-stack-init addr>data ,
 
-\ FIXME: remove
-label xxx
-\	sendZero temp1 mov,
-	\ temp1 have to be filled with data that should be transmitted
-	temp0 UCSR0A in/lds, temp0 5 sbrs, xxx rjmp, \ wait for empty transmit buffer
-	temp1 UDR0 out/sts, \ put data (temp1) into buffer sends the data
-label xxx2
-	temp1 $0a ldi,
-	temp0 UCSR0A in/lds, temp0 5 sbrs, xxx2 rjmp, \ wait for empty transmit buffer
-	\ FIXME: that should be swapped
-	temp1 UDR0 out/sts, \ put data (temp1) into buffer sends the data
-	ret,
-    here jmp-reset jmp!
-end-label+
 
 include avr/usart-init.fs
 include avr/timer0-ticks.fs
